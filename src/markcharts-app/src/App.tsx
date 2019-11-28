7fe42b6a959a251f2deadf737b15f5b3eb3e86b9
Damien Flury <damien.flury@gmail.com>
2019-11-28 12:59:58 +0100

    Add eslint

--- src/markcharts-app/src/App.tsx
+++ src/markcharts-app/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'emotion-theming';
+import { Theme } from './styled';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const theme: Theme = {
+  colors: {
+    primary: '#ff33ff',
+  },
+};
+
+const App: React.FC = () => (
+  <ThemeProvider theme={theme}>
+    <div>App</div>
+  </ThemeProvider>
+);
 
 export default App;
